4a17829124b5ad2d5feb47e97eaddd5ada5988a4
Nirvan004 <Nirvanandb2004@gmail.com>
2026-01-16 13:55:18 -0500

    finished task 3

---
+++ Product-api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/connection");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
